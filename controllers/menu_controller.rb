17e0d4704aee03830c1c7ddbd7e07d0b9e509f00
Dominick <dominick@theosfp.com>
2017-04-24 11:00:27 -0700

    Fixed opening and closings and indentation

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -16,66 +16,66 @@ class MenuController
     puts "5 - Exit"
     print "Enter your selection: "
     selection = gets.to_i
-        case selection
-          when 1
-            system "clear"
-            view_all_entries
-            main_menu
-          when 2
-            system "clear"
-            create_entry
-            main_menu
-          when 3
-            system "clear"
-            search_entries
-            main_menu
-          when 4
-            system "clear"
-            read_csv
-            main_menu
-          when 5
-            puts "Good-bye!"
-            exit(0)
-          else
-            system "clear"
-            puts "Sorry, that is not a valid input"
-            main_menu
-        end
-      end
+    case selection
+    when 1
+      system "clear"
+      view_all_entries
+      main_menu
+    when 2
+      system "clear"
+      create_entry
+      main_menu
+    when 3
+      system "clear"
+      search_entries
+      main_menu
+    when 4
+      system "clear"
+      read_csv
+      main_menu
+    when 5
+      puts "Good-bye!"
+      exit(0)
+    else
+      system "clear"
+      puts "Sorry, that is not a valid input"
+      main_menu
+    end
+  end
 
-      def view_all_entries
-        address_book.entries.each do |entry|
-          system "clear"
-          puts entry.to_s
-          entry_submenu(entry)
-        end
+  def view_all_entries
+    address_book.entries.each do |entry|
+      system "clear"
+      puts entry.to_s
+      entry_submenu(entry)
+    end
 
-        system "clear"
-        puts "End of entries"
-      end
+    system "clear"
+    puts "End of entries"
+  end
 
-      def create_entry
-        system "clear"
-        puts "New AddressBloc Entry"
-        print "Name: "
-        name = gets.chomp
+  def create_entry
+    system "clear"
+    puts "New AddressBloc Entry"
+    print "Name: "
+    name = gets.chomp
 
-        print "Phone number: "
-        phone = gets.chomp
+    print "Phone number: "
+    phone = gets.chomp
 
-        print "Email: "
-        email = gets.chomp
+    print "Email: "
+    email = gets.chomp
 
-        address_book.add_entry(name, phone, email)
+    address_book.add_entry(name, phone, email)
 
-        system "clear"
-        puts "New entry created"
-      end
+    system "clear"
+    puts "New entry created"
+  end
 
-      def search_entries
-      end
+  def search_entries
+  end
 
-      def read_csv
+  def read_csv
   end
 
   def entry_submenu(entry)
@@ -87,16 +87,16 @@ class MenuController
     selection = gets.chomp
 
     case selection
-      when "n"
-      when "d"
-      when "e"
-      when "m"
-        system "clear"
-        main_menu
-      else
-        system "clear"
-        puts "#{selection} is not a valid input"
-        entry_submenu(entry)
+    when "n"
+    when "d"
+    when "e"
+    when "m"
+      system "clear"
+      main_menu
+    else
+      system "clear"
+      puts "#{selection} is not a valid input"
+      entry_submenu(entry)
     end
   end
 end
