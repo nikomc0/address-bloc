1c1d621565135fa53060c68e2cf94d784de2a42d
Dominick <dominick@theosfp.com>
2017-04-24 11:13:38 -0700

    checkpoint-5 merge

--- models/address_book.rb
+++ models/address_book.rb
@@ -27,6 +27,8 @@ require "csv"
       csv.each do |row|
         row_hash = row.to_hash
         add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+      end
+    end
 
     def remove_entry (name, phone_number, email)
       entry_index = nil
