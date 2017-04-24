fa68e474d56e6068b7601bac1e481a833ba26f79
Dominick <dominick@theosfp.com>
2017-04-23 20:48:25 -0700

    Checkpoint-6 CSV

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative 'entry'
+require "csv"
 
   class AddressBook
     attr_reader :entries
@@ -20,17 +21,13 @@ require_relative 'entry'
      entries.insert(index, Entry.new(name, phone_number, email))
     end
 
-    def remove_entry (name, phone_number, email)
-      entryIndex = nil
-
-      entries.each_with_index do |entry, index|
-        if name == entry.name && phone_number == entry.phone_number && email == entry.email
-          entryIndex = index
-        end
-
-        if entryIndex
-          entries.delete_at(entryIndex)
-        end
+    def import_from_csv(file_name)
+      csv_text = File.read(file_name)
+      csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+      csv.each do |row|
+        row_hash = row.to_hash
+        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
       end
     end
+
   end
