e8eab3e7a62f8fbe88de67b4167e344e60c0e437
Dominick <dominick@theosfp.com>
2017-04-25 16:13:13 -0700

    Checkpoint-7-Binary-Search

--- models/address_book.rb
+++ models/address_book.rb
@@ -61,14 +61,4 @@ require "csv"
       end
       return nil
     end
-
-    def iterative_search(name)
-      entries.each do |entry|
-        if entry.name == name
-          return entry
-        end
-      end
-
-      return nil
-    end
   end
